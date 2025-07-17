c3ee71d0a68a1544bd097da3c828f71be3fade52
Geniusguy24 <ekanshpardeshi2412@gmail.com>
2025-07-17 20:53:14 +0530

    first commit

---
+++ SimpleLists.py
@@ -0,0 +1,17 @@
+empty_list = []
+
+print(empty_list)
+
+number_list = [1, 2, 3, 4, 5]
+
+print(number_list)
+
+triples = [1, 2, 3] * 3
+
+print(triples)
+
+aList = [100, 200, 300, 400, 500]
+
+aList = aList[::-1]
+
+print(aList, "\n")
